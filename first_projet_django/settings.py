d102fdd22177b4c41226bee40e7e9de575e440ef
Vlad <vladrymarchuk@gmail.com>
2023-01-05 20:58:52 +0200

    changed .env and readme.md

--- first_projet_django/settings.py
+++ first_projet_django/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import environ
 
 
 
@@ -26,11 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
+env = environ.Env()
+environ.Env.read_env()
 
 ALLOWED_HOSTS = []
 
