043714f89c95d7f147c8814a6ac73f59e5a05ec6
Vlad <vladrymarchuk@gmail.com>
2023-01-05 21:01:27 +0200

    changed settings.py(secret key and debug)

--- first_projet_django/settings.py
+++ first_projet_django/settings.py
@@ -26,8 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-env = environ.Env()
-environ.Env.read_env()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
 
 ALLOWED_HOSTS = []
 
